aabf33c16ba02f320ff482cb8159e2b0d5182af1
Clive Cox <cc@seldon.io>
2018-02-06 18:11:09 +0000

    add training shell script

---
+++ model/train/build_and_push.sh
@@ -0,0 +1,13 @@
+SRC=$1
+REPO=$2
+
+until docker ps; 
+do sleep 3; 
+done; 
+
+cd ${SRC}/model/train 
+docker build --force-rm=true -t ${REPO}/deepmnistclassifier_trainer:0.1 . 
+docker images 
+echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
+docker push ${REPO}/deepmnistclassifier_trainer:`cat VERSION`
+
