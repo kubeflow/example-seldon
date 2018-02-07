0eab3583262e23a0f5626cf69f811a7020c464d8
Clive Cox <cc@seldon.io>
2018-02-07 14:02:39 +0000

    add version to build and push script as argument

--- model/train/build_and_push.sh
+++ model/train/build_and_push.sh
@@ -1,11 +1,13 @@
-REPO=$1
+VERSION=$1
+REPO=$2
 
 until docker ps; 
 do sleep 3; 
 done; 
 
-docker build --force-rm=true -t ${REPO}/deepmnistclassifier_trainer:0.1 . 
+docker build --force-rm=true -t ${REPO}/deepmnistclassifier_trainer:${VERSION} . 
 docker images 
 echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
-docker push ${REPO}/deepmnistclassifier_trainer:`cat VERSION`
+docker push ${REPO}/deepmnistclassifier_trainer:${VERSION}
+
 
