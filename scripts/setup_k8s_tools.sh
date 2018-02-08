fa6ad9b55b9c2a6b98ae3d9014de431fb2374066
Clive Cox <cc@seldon.io>
2018-02-08 16:29:41 +0000

    add missing folders

---
+++ scripts/setup_k8s_tools.sh
@@ -0,0 +1,13 @@
+ks init k8s_tools --api-spec=version:v1.8.0
+cd k8s_tools
+ks registry add kubeflow github.com/kubeflow/kubeflow/tree/master/kubeflow 
+ks pkg install kubeflow/core 
+ks pkg install kubeflow/tf-serving 
+ks pkg install kubeflow/tf-job 
+kubectl create clusterrolebinding default-admin --clusterrole=cluster-admin --user=<user-email>
+ks generate core kubeflow-core --name=kubeflow-core
+ks registry add seldon-core github.com/SeldonIO/seldon-core/tree/master/seldon-core
+ks pkg install seldon-core/seldon-core
+ks generate seldon-core seldon-core --withApife=false --withRbac=true
+ks pkg install kubeflow/argo
+ks prototype use io.ksonnet.pkg.argo argo --namespace default --name argo
