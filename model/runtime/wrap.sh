ad69722c86ecc1247ffb61f3b66da744c0dce82e
Clive Cox <cc@seldon.io>
2018-02-08 10:11:23 +0000

    make wrap.sh use sh syntax

--- model/runtime/wrap.sh
+++ model/runtime/wrap.sh
@@ -5,9 +5,7 @@ until docker ps;
 do sleep 3; 
 done; 
 
-STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-docker run -v  ${STARTUP_DIR}:/my_model seldonio/core-python-wrapper:0.6 /my_model DeepMnist ${VERSION} seldonio --image-name=deepmnistclassifier_runtime
+docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.6 /my_model DeepMnist ${VERSION} seldonio --image-name=deepmnistclassifier_runtime
 docker images 
 echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
 docker push ${REPO}/deepmnistclassifier_runtime:${VERSION}
