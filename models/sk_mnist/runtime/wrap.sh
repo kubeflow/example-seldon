885617d3e941114489347470fee5e8f5c49fe7ea
Clive Cox <cc@seldon.io>
2018-02-12 12:34:17 +0000

    missing repo in sk learn runtime

--- models/sk_mnist/runtime/wrap.sh
+++ models/sk_mnist/runtime/wrap.sh
@@ -5,7 +5,7 @@ until docker ps;
 do sleep 3; 
 done; 
 
-docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.6 /my_model SkMnist ${VERSION} seldonio --image-name=skmnistclassifier_runtime
+docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.6 /my_model SkMnist ${VERSION} ${REPO} --image-name=skmnistclassifier_runtime
 cd ./build
 ./build_image.sh
 docker images 
