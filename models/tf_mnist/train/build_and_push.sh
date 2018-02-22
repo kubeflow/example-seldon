be93a5f18455fb709a5cc3294c7653a881e3684c
Clive Cox <cc@seldon.io>
2018-02-22 09:58:22 +0000

    update build and push script with push argument

--- models/tf_mnist/train/build_and_push.sh
+++ models/tf_mnist/train/build_and_push.sh
@@ -1,13 +1,18 @@
 VERSION=$1
 REPO=$2
+PUSH=$3
+
+IMAGE=deepmnistclassifier_trainer
 
 until docker ps; 
 do sleep 3; 
 done; 
 
-docker build --force-rm=true -t ${REPO}/deepmnistclassifier_trainer:${VERSION} . 
+docker build --force-rm=true -t ${REPO}/${IMAGE}:${VERSION} . 
 docker images 
-echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
-docker push ${REPO}/deepmnistclassifier_trainer:${VERSION}
-
+if [$PUSH = "true"] then
+   echo "Pushing image to ${REPO}/${IMAGE}:${VERSION}"
+   echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
+   docker push ${REPO}/${IMAGE}:${VERSION}
+fi
 
