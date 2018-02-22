1c5771c39705e27777e8ec227940527cc48c8cc6
Clive Cox <cc@seldon.io>
2018-02-22 10:43:20 +0000

    update sk build_and_push

--- models/sk_mnist/train/build_and_push.sh
+++ models/sk_mnist/train/build_and_push.sh
@@ -1,13 +1,18 @@
 VERSION=$1
 REPO=$2
+PUSH=$3
+
+IMAGE=skmnistclassifier_trainer
 
 until docker ps; 
 do sleep 3; 
 done; 
 
-docker build --force-rm=true -t ${REPO}/skmnistclassifier_trainer:${VERSION} . 
+docker build --force-rm=true -t ${REPO}/${IMAGE}:${VERSION} . 
 docker images 
-echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
-docker push ${REPO}/skmnistclassifier_trainer:${VERSION}
-
+if test "$PUSH" = 'true'; then
+   echo "Pushing image to ${REPO}/${IMAGE}:${VERSION}"
+   echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
+   docker push ${REPO}/${IMAGE}:${VERSION}
+fi
 
