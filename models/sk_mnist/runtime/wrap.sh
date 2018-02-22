96c3afaf5f8abf9c135e5e6f0d02c7c799954858
Clive Cox <cc@seldon.io>
2018-02-22 13:39:50 +0000

    update sk model wrap

--- models/sk_mnist/runtime/wrap.sh
+++ models/sk_mnist/runtime/wrap.sh
@@ -1,16 +1,22 @@
 VERSION=$1
 REPO=$2
+PUSH=$3
+
+IMAGE=skmnistclassifier_runtime
 
 until docker ps; 
 do sleep 3; 
 done; 
 
-docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.6 /my_model SkMnist ${VERSION} ${REPO} --image-name=skmnistclassifier_runtime
+docker run -v  ${PWD}:/my_model seldonio/core-python-wrapper:0.6 /my_model SkMnist ${VERSION} ${REPO} --image-name=${IMAGE}
 cd ./build
 ./build_image.sh
 docker images 
-echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
-./push_image.sh
+if test "$PUSH" = 'true'; then
+    echo "Pushing image to ${REPO}/${IMAGE}:${VERSION}"
+    echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
+    ./push_image.sh
+fi
 
 
 
