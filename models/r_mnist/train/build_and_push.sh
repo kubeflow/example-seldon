292ef784a9fd751c09450686c93ac4307d4eefb7
Clive Cox <cc@seldon.io>
2018-04-15 18:17:53 +0100

    add R example model

---
+++ models/r_mnist/train/build_and_push.sh
@@ -0,0 +1,14 @@
+VERSION=$1
+REPO=$2
+
+IMAGE=rmnistclassifier_trainer
+
+until docker ps; 
+do sleep 3; 
+done; 
+
+docker build --force-rm=true -t ${REPO}/${IMAGE}:${VERSION} . 
+docker images 
+echo "Pushing image to ${REPO}/${IMAGE}:${VERSION}"
+echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin 
+docker push ${REPO}/${IMAGE}:${VERSION}
