8fdb20f6db84ead2d4c33821fcba3cb528966c2a
Clive Cox <cc@seldon.io>
2018-04-27 17:23:04 +0100

    docker test

--- models/sk_mnist/runtime/wrap.sh
+++ models/sk_mnist/runtime/wrap.sh
@@ -6,6 +6,10 @@ IMAGE=skmnistclassifier_runtime
 echo "start"
 echo $DOCKER_HOST
 
+export DOCKER_HOST="tcp://127.0.0.1"
+
+echo $DOCKER_HOST
+
 apk add --update openssl
 
 wget https://github.com/openshift/source-to-image/releases/download/v1.1.9a/source-to-image-v1.1.9a-40ad911d-linux-amd64.tar.gz
