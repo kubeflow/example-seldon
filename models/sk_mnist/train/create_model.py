5d10fb1991a22675ac9c9f47fa45d5f54694da26
Clive Cox <cc@seldon.io>
2018-08-19 20:09:38 +0100

    Update scripts for latest kubeflow install

--- models/sk_mnist/train/create_model.py
+++ models/sk_mnist/train/create_model.py
@@ -3,15 +3,37 @@ from sklearn import datasets, metrics
 from sklearn.utils import shuffle
 from sklearn.datasets import fetch_mldata
 from sklearn.externals import joblib
+from six.moves import urllib
 
 if __name__ == '__main__':
+    try:
+        mnist = fetch_mldata('MNIST original')
+    except urllib.error.HTTPError as ex:
+        print("Could not download MNIST data from mldata.org, trying alternative...")
 
-    mnist = fetch_mldata('MNIST original', data_home="./mnist_sklearn")
+        # Alternative method to load MNIST, if mldata.org is down
+        from scipy.io import loadmat
+        mnist_alternative_url = "https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat"
+        mnist_path = "./mnist-original.mat"
+        response = urllib.request.urlopen(mnist_alternative_url)
+        with open(mnist_path, "wb") as f:
+            content = response.read()
+            f.write(content)
+        mnist_raw = loadmat(mnist_path)
+        mnist = {
+            "data": mnist_raw["data"].T,
+            "target": mnist_raw["label"][0],
+            "COL_NAMES": ["label", "data"],
+            "DESCR": "mldata.org dataset: mnist-original",
+        }
+        print("Success!")
+
+    #mnist = fetch_mldata('MNIST original', data_home="./mnist_sklearn")
     # To apply a classifier on this data, we need to flatten the image, to
     # turn the data in a (samples, feature) matrix:
-    n_samples = len(mnist.data)
-    data = mnist.data.reshape((n_samples, -1))
-    targets = mnist.target
+    n_samples = len(mnist['data'])
+    data = mnist['data'].reshape((n_samples, -1))
+    targets = mnist['target']
 
     data,targets = shuffle(data,targets)
     classifier = RandomForestClassifier(n_estimators=30)
